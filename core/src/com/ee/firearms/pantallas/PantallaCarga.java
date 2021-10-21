151ec1bc90b90004d21b40952ed0127232c26de7
Erik Estrada <erikestradaf21@gmail.com>
2021-10-21 10:46:14 -0300

    Second Commit

---
+++ core/src/com/ee/firearms/pantallas/PantallaCarga.java
@@ -0,0 +1,91 @@
+package com.ee.firearms.pantallas;
+
+import com.badlogic.gdx.Screen;
+import com.badlogic.gdx.graphics.g2d.SpriteBatch;
+import com.ee.firearms.elementos.Imagen;
+import com.ee.firearms.utiles.Recursos;
+import com.ee.firearms.utiles.Render;
+
+public class PantallaCarga implements Screen {
+	Imagen fondo;
+	SpriteBatch b;
+	boolean fadeInTerminado = false, termina = false;
+	float a = 0;
+	float contTiempo = 0, tiempoEspera = 5;
+	float contTiempoTermina = 0, tiempoTermina = 5;
+	
+	@Override
+	public void show() {
+		fondo = new Imagen(Recursos.LOGO);
+		fondo.setSize(1000, 640);
+		b = Render.batch;
+		fondo.setTransparencia(1);
+		
+	}
+
+	@Override
+	public void render(float delta) {
+		Render.limpiarPantalla(0,0,0,0);
+		
+		b.begin();
+		fondo.dibujar();
+		b.end();
+		
+		procesarFade();
+	}
+
+	private void procesarFade() {
+		if (!fadeInTerminado) {
+			a += 0.01f;
+			if (a > 1) {
+				a = 1;
+				fadeInTerminado = true;
+			}
+		} else {
+			contTiempo += 0.05f;
+			if (contTiempo > tiempoEspera) {
+				a -= 0.01f;
+				if (a < 0) {
+					a = 0;
+					termina = true;
+				}
+			}
+		}
+		System.out.println(a);
+		fondo.setTransparencia(a);
+		
+		if (termina) {
+			contTiempoTermina += 0.01f;
+			if (contTiempoTermina > tiempoTermina) {
+//				System.out.println("CAMBIO DE PANTALLA");
+				Render.app.setScreen(new PantallaMenu());
+			}
+		}
+		
+	}
+
+	@Override
+	public void resize(int width, int height) {
+		
+	}
+
+	@Override
+	public void pause() {
+		
+	}
+
+	@Override
+	public void resume() {
+		
+	}
+
+	@Override
+	public void hide() {
+		
+	}
+
+	@Override
+	public void dispose() {
+		
+	}
+}
---
+++ core/src/com/ee/firearms/pantallas/PantallaUnJugador.java
@@ -0,0 +1,50 @@
+package com.ee.firearms.pantallas;
+
+import com.badlogic.gdx.Screen;
+import com.ee.firearms.elementos.Guerrero;
+import com.ee.firearms.utiles.Render;
+
+public class PantallaUnJugador implements Screen {
+
+	Guerrero g,g2;
+	@Override
+	public void show() {
+		//g = new Guerrero(200,200);
+		g2 = new Guerrero(400,200); 
+	}
+
+	@Override
+	public void render(float delta) {
+		Render.limpiarPantalla(0, 0, 0, 0);
+		Render.batch.begin();
+		//g.render(Render.batch);
+		g2.render(Render.batch);
+		Render.batch.end();
+	}
+
+	@Override
+	public void resize(int width, int height) {
+		
+	}
+
+	@Override
+	public void pause() {
+		
+	}
+
+	@Override
+	public void resume() {
+		
+	}
+
+	@Override
+	public void hide() {
+		
+	}
+
+	@Override
+	public void dispose() {
+		
+	}
+
+}
